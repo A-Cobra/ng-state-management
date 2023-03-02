bdd4e237199f1d0e4a4a7626a46663bb6cee0c1f
Mauricio Castro <mcastro@applaudostudios.com>
2023-03-02 17:58:14 -0600

    fix(typeorm): Scripts for migrations
    Use typeorm.config.ts to connect to database through package.json
    scripts, will compare entities in the whole project vs current
    database to notice changes and generate appropriate migrations
    
    Currently there are some magic strings that need to be fixed
    but it works.

--- packages/state-management-backend/src/app/database/typeorm.config.ts
+++ packages/state-management-backend/src/app/database/typeorm.config.ts
@@ -1,16 +1,18 @@
+// this whole file is what the package.json scripts use to connect to the DB without running NestJS,
+// PostgreSQL must be running either standalone or in a container
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { ConfigService } from '@nestjs/config';
 import { config } from 'dotenv';
 import { getEnvPath } from '../common/utils/env-path';
 
-const envFilePath: string = getEnvPath(process.env.WORKDIR);
-
+const envFilePath: string = getEnvPath(`${__dirname}/../../../../..`);
+console.log(envFilePath)
 config({ path: envFilePath });
 
 const configService = new ConfigService();
 const options: DataSourceOptions = {
   type: 'postgres',
-  host: configService.get('DB_HOST'),
+  host: 'localhost',
   port: configService.get('DB_PORT'),
   username: configService.get('DB_USER'),
   password: configService.get('DB_PASS'),
@@ -18,4 +20,6 @@ const options: DataSourceOptions = {
   entities: [`${__dirname}/../**/*.entity.{js,ts}`],
   migrations: [`${__dirname}/migrations/*.ts`],
 };
+
+// console.log(options)
 export default new DataSource(options);
