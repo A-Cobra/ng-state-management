263a7596bc87b38e54df2e6beed9fb8c1b598cab
Mauricio Castro <mcastro@applaudostudios.com>
2023-03-02 15:29:20 -0600

    feat(validation): Add global validation for DTOs
    Setup global pipe to strip invalid fields and transform inputs from
    requests for DTO usage.
    
    Setup API versioning

--- packages/state-management-backend/src/main.ts
+++ packages/state-management-backend/src/main.ts
@@ -3,7 +3,7 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
@@ -11,8 +11,21 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // general options
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    })
+  );
+
+  // server setup
   const port = process.env.API_PORT || 4200;
   await app.listen(port);
   Logger.log(
