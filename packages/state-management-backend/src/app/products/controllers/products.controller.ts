22695d773d23a2d92e46a6e720454304927974f4
xhugoyg <hsolano@applaudostudios.dev>
2023-03-17 10:27:08 -0600

    add ProductsController and the prisma Product model based on the schema using the @map to persist the cammelCase on the app but the snake_case on the db

---
+++ packages/state-management-backend/src/app/products/controllers/products.controller.ts
@@ -0,0 +1,32 @@
+import { Body, Controller, Get, Param, Post, Put, Patch } from '@nestjs/common';
+
+@Controller({
+  path: 'products',
+  version: '1',
+})
+export class ProductsController {
+  @Get()
+  getAllProducts(): string {
+    return 'Get All Products';
+  }
+
+  @Get(':id')
+  getProduct(@Param('id') id: string): string {
+    return `Get a specific Product with a specific id ${id}`;
+  }
+
+  @Post()
+  createProduct(@Body() body: any): string {
+    return 'Post a new product';
+  }
+
+  @Put(':id')
+  updateProduct(@Param('id') id: string, @Body() body: any): string {
+    return `PUT a specific product ${id}`;
+  }
+
+  @Patch(':id')
+  partialUpdateProduct(@Param('id') id: string, @Body() body: any): string {
+    return `PATCH a specific product${id}`;
+  }
+}
--- packages/state-management-backend/src/app/products/products.module.ts
+++ packages/state-management-backend/src/app/products/products.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ProductsController } from './controllers/products.controller';
 import { ReviewsModule } from './reviews/reviews.module';
 
-@Module({ imports: [ReviewsModule] })
+@Module({ imports: [ReviewsModule], controllers: [ProductsController] })
 export class ProductsModule {}
