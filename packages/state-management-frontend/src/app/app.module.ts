9fd8833d57d5db5a89296b6b782e3ed4d777d9b3
Jaime Alejandro <jxa.omega@gmail.com>
2023-03-21 16:06:55 -0600

    removing unused import

--- packages/state-management-frontend/src/app/app.module.ts
+++ packages/state-management-frontend/src/app/app.module.ts
@@ -1,10 +1,9 @@
-import { AppRoutingModule } from './app-routing.module';
-import { BusinessModule } from './business/business.module';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import {AppComponent} from './app.component';
+import {NxWelcomeComponent} from './nx-welcome.component';
 
 
 @NgModule({
@@ -13,4 +12,5 @@ import { NxWelcomeComponent } from './nx-welcome.component';
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
