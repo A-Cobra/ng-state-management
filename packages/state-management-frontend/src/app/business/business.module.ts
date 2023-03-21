0e295a58a0d0ae5849af9f137ae6e86074058db2
Cobra <conradbravina@gmail.com>
2023-03-20 19:26:00 -0500

    Added the basic form structure
    It doesn't yet use CLAPP

--- packages/state-management-frontend/src/app/business/business.module.ts
+++ packages/state-management-frontend/src/app/business/business.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BusinessRoutingModule } from './business-routing.module';
 import { BusinessEditComponent } from './components/business-edit/business-edit.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [BusinessEditComponent],
-  imports: [CommonModule, BusinessRoutingModule],
+  imports: [CommonModule, BusinessRoutingModule, ReactiveFormsModule],
 })
 export class BusinessModule {}
--- packages/state-management-frontend/src/app/business/components/business-edit/business-edit.component.ts
+++ packages/state-management-frontend/src/app/business/components/business-edit/business-edit.component.ts
@@ -1,8 +1,41 @@
 import { Component } from '@angular/core';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'state-management-app-business-edit',
   templateUrl: './business-edit.component.html',
   styleUrls: ['./business-edit.component.scss'],
 })
-export class BusinessEditComponent {}
+export class BusinessEditComponent {
+  editing = false;
+
+  businessFormEdit = this.formBuilder.group({
+    displayName: ['', [Validators.required]],
+    businessName: ['', [Validators.required]],
+    businessClassification: ['1', []],
+    contactPhoneNumber: ['987654321', []],
+    contactEmail: [
+      'name@domain.suffix',
+      [
+        /*CustomFormValidators.email*/
+      ],
+    ],
+    contactAddress: ['Address', []],
+    longitude: ['Longitude', []],
+    latitude: ['Latitude', []],
+  });
+
+  constructor(private formBuilder: NonNullableFormBuilder){}
+
+  onEditClick() {
+    this.toggleEditingStatus();
+  }
+
+  onSaveClick() {
+    this.toggleEditingStatus();
+  }
+
+  toggleEditingStatus() {
+    this.editing = !this.editing;
+  }
+}
