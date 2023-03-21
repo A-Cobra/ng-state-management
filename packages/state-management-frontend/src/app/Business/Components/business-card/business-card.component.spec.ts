a1e869d546fab00e1a9104dfb647ed2ce7750a12
Jaime Alejandro <jxa.omega@gmail.com>
2023-03-21 11:50:29 -0600

    testing

--- packages/state-management-frontend/src/app/Business/Components/business-card/business-card.component.spec.ts
+++ packages/state-management-frontend/src/app/Business/Components/business-card/business-card.component.spec.ts
@@ -1,6 +1,7 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { BusinessCardComponent } from './business-card.component';
+import {BusinessCardComponent} from './business-card.component';
+import {ClappButtonModule, ClappCardModule, ClappPaginationModule, ClappSearchModule} from "@clapp1/clapp-angular";
 
 describe('BusinessCardComponent', () => {
   let component: BusinessCardComponent;
@@ -9,6 +10,10 @@ describe('BusinessCardComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [BusinessCardComponent],
+      imports: [ClappButtonModule,
+        ClappSearchModule,
+        ClappPaginationModule,
+        ClappCardModule,]
     }).compileComponents();
 
     fixture = TestBed.createComponent(BusinessCardComponent);
--- packages/state-management-frontend/src/app/Business/Components/business-list/business-list.component.spec.ts
+++ packages/state-management-frontend/src/app/Business/Components/business-list/business-list.component.spec.ts
@@ -1,6 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { BusinessListComponent } from './business-list.component';
+import {BusinessListComponent} from './business-list.component';
+import {
+  ClappButtonModule,
+  ClappCardModule,
+  ClappPaginationModule,
+  ClappSearchModule,
+} from "@clapp1/clapp-angular";
+import {Component, Input} from "@angular/core";
+
+@Component({
+  selector: "state-management-app-business-card",
+  template: "",
+})
+class MockBusinessCardComponent {
+  @Input() businessId: number;
+}
 
 describe('BusinessListComponent', () => {
   let component: BusinessListComponent;
@@ -8,7 +23,11 @@ describe('BusinessListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [BusinessListComponent],
+      declarations: [BusinessListComponent, MockBusinessCardComponent,],
+      imports: [ClappButtonModule,
+        ClappSearchModule,
+        ClappPaginationModule,
+        ClappCardModule,]
     }).compileComponents();
 
     fixture = TestBed.createComponent(BusinessListComponent);
