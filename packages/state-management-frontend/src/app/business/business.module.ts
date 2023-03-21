09ea08475c7325f91b26b29fa6ab6f125e2bdc53
Cobra <conradbravina@gmail.com>
2023-03-20 19:51:16 -0500

    Started experimenting with the Clapp library

--- packages/state-management-frontend/src/app/business/business.module.ts
+++ packages/state-management-frontend/src/app/business/business.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BusinessRoutingModule } from './business-routing.module';
 import { BusinessEditComponent } from './components/business-edit/business-edit.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ClappTextInputModule } from '@clapp1/clapp-angular';
 
 @NgModule({
   declarations: [BusinessEditComponent],
-  imports: [CommonModule, BusinessRoutingModule, ReactiveFormsModule],
+  imports: [CommonModule, BusinessRoutingModule, ReactiveFormsModule, ClappTextInputModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class BusinessModule {}
