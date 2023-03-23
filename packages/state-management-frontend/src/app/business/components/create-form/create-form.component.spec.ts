87cf68590f552d5b3abe1f83d720d06e116f70c7
Esteban Manrupe <emanrupe@applaudostudios.com>
2023-03-23 18:23:28 -0300

    Add ModalModule on test

--- packages/state-management-frontend/src/app/business/components/create-form/create-form.component.spec.ts
+++ packages/state-management-frontend/src/app/business/components/create-form/create-form.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { CreateFormComponent } from './create-form.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ModalModule } from '@clapp1/clapp-angular';
 
 describe('CreateFormComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, ModalModule],
       declarations: [CreateFormComponent],
     }).compileComponents();
   });
