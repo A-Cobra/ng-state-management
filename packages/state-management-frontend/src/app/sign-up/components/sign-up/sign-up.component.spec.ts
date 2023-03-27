fe52ab1487b40a8ad647f7fc22bd5b39f31fd436
JBRIZO <jlaux@applaudostudios.com>
2023-03-27 16:07:11 -0600

    Tests for sign-up module

--- packages/state-management-frontend/src/app/sign-up/components/sign-up/sign-up.component.spec.ts
+++ packages/state-management-frontend/src/app/sign-up/components/sign-up/sign-up.component.spec.ts
@@ -1,22 +1,126 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormBuilder } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { NotificationService } from '@clapp1/clapp-angular';
+import { of } from 'rxjs';
+import { UserI } from '../../interfaces/user.interface';
+import { SignUpService } from '../../services/sign-up.service';
 import { SignUpComponent } from './sign-up.component';
 
 describe('SignUpComponent', () => {
   let component: SignUpComponent;
-  let fixture: ComponentFixture<SignUpComponent>;
+  let formBuilder: FormBuilder;
+  let titleService: Title;
+  let signUpService: SignUpService;
+  let router: Router;
+  let notificationService: NotificationService;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [SignUpComponent],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(SignUpComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => {
+    formBuilder = new FormBuilder();
+    titleService = new Title(document);
+    signUpService = new SignUpService();
+    router = { navigate: jest.fn() } as any;
+    notificationService = { success: jest.fn(), error: jest.fn() } as any;
+    component = new SignUpComponent(
+      formBuilder,
+      titleService,
+      signUpService,
+      router,
+      notificationService
+    );
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('ngOnInit', () => {
+    it('should initialize the form with empty fields', () => {
+      component.ngOnInit();
+      expect(component.signUpForm.value).toEqual({
+        firstName: '',
+        lastName: '',
+        userName: '',
+        email: '',
+        contactNumber: '',
+        password: '',
+        confirmPassword: '',
+      });
+    });
+
+    it('should set the page title', () => {
+      component.ngOnInit();
+      expect(titleService.getTitle()).toEqual('Sign up');
+    });
+  });
+
+  describe('onSubmit', () => {
+    beforeEach(() => {
+      component.ngOnInit();
+    });
+
+    it('should set passwordMismatch error if passwords do not match', () => {
+      component.password?.setValue('password1');
+      component.confirmPassword?.setValue('password2');
+      component.onSubmit();
+      expect(component.confirmPassword?.errors).toEqual({
+        passwordMismatch: true,
+      });
+    });
+
+    it('should not set passwordMismatch error if passwords match', () => {
+      component.password?.setValue('password');
+      component.confirmPassword?.setValue('password');
+      component.onSubmit();
+      expect(component.confirmPassword?.errors).toBeNull();
+    });
+
+    it('should call signUpService with user details and navigate to login page on success', () => {
+      const user: UserI = {
+        name: 'John',
+        lastName: 'Doe',
+        username: 'johndoe',
+        email: 'johndoe@example.com',
+        contactNumber: '1234567890',
+        password: 'password',
+      };
+      component.signUpForm.setValue({
+        firstName: user.name,
+        lastName: user.lastName,
+        userName: user.username,
+        email: user.email,
+        contactNumber: user.contactNumber,
+        password: user.password,
+        confirmPassword: user.password,
+      });
+      jest.spyOn(signUpService, 'signUp').mockReturnValueOnce(of(true));
+
+      component.onSubmit();
+
+      expect(signUpService.signUp).toHaveBeenCalledWith(user);
+      expect(router.navigate).toHaveBeenCalledWith(['/login']);
+    });
+
+    it('should not navigate on failure', () => {
+      const user: UserI = {
+        name: 'John',
+        lastName: 'Doe',
+        username: 'johndoe',
+        email: 'johndoe@example.com',
+        contactNumber: '1234567890',
+        password: 'password',
+      };
+      component.signUpForm.setValue({
+        firstName: user.name,
+        lastName: user.lastName,
+        userName: user.username,
+        email: user.email,
+        contactNumber: user.contactNumber,
+        password: user.password,
+        confirmPassword: user.password,
+      });
+      jest.spyOn(signUpService, 'signUp').mockReturnValueOnce(of(false));
+
+      component.onSubmit();
+
+      expect(signUpService.signUp).toHaveBeenCalledWith(user);
+      expect(router.navigate).not.toHaveBeenCalled();
+    });
   });
 });
--- packages/state-management-frontend/src/app/sign-up/services/sign-up.service.spec.ts
+++ packages/state-management-frontend/src/app/sign-up/services/sign-up.service.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed } from '@angular/core/testing';
-
 import { SignUpService } from './sign-up.service';
 
 describe('SignUpService', () => {
   let service: SignUpService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [SignUpService],
+    });
     service = TestBed.inject(SignUpService);
   });
 
