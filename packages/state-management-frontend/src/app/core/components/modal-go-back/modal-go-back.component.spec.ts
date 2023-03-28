c642cddbc5176688e164f8f538ef54184ead6a30
Conrad Bravo <cbravo@applaudostudios.com>
2023-03-28 13:08:29 -0500

    Added a mock to pass the tests
    Note that it yet has to be completelly implemented with logic

--- packages/state-management-frontend/src/app/core/components/modal-go-back/modal-go-back.component.spec.ts
+++ packages/state-management-frontend/src/app/core/components/modal-go-back/modal-go-back.component.spec.ts
@@ -1,14 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ModalService } from '@clapp1/clapp-angular';
+import { MockModalService } from '../../../business/models/mock-modal-service.interface';
 
 import { ModalGoBackComponent } from './modal-go-back.component';
 
 describe('ModalSureToLeaveComponent', () => {
   let component: ModalGoBackComponent;
   let fixture: ComponentFixture<ModalGoBackComponent>;
+  let mockModalService: MockModalService;
 
   beforeEach(async () => {
+    mockModalService = {
+      open: jest.fn(),
+      close: jest.fn(),
+    };
     await TestBed.configureTestingModule({
       declarations: [ModalGoBackComponent],
+      providers: [
+        {
+          provide: ModalService,
+          useValue: mockModalService,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ModalGoBackComponent);
