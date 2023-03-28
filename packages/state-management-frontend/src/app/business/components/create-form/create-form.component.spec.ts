af6f7c6ae5eb9fc419749ac7b6fce4bae758a827
Esteban Manrupe <emanrupe@applaudostudios.com>
2023-03-28 12:45:49 -0300

    fix spec files to allow push

--- packages/state-management-frontend/src/app/business/components/create-form/create-form.component.spec.ts
+++ packages/state-management-frontend/src/app/business/components/create-form/create-form.component.spec.ts
@@ -1,12 +1,31 @@
 import { TestBed } from '@angular/core/testing';
 import { CreateFormComponent } from './create-form.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
-import { ModalModule } from '@clapp1/clapp-angular';
+import {
+  ClappButtonModule,
+  ClappTextInputModule,
+  ClappInputHelpersModule,
+  ClappSelectModule,
+  ClappImageDisplayModule,
+  ModalModule,
+  ClappNotificationModule,
+} from '@clapp1/clapp-angular';
 
 describe('CreateFormComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, ModalModule],
+      imports: [
+        RouterTestingModule,
+        ClappButtonModule,
+        ClappTextInputModule,
+        ClappInputHelpersModule,
+        ClappSelectModule,
+        ClappImageDisplayModule,
+        ModalModule,
+        ClappNotificationModule,
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
       declarations: [CreateFormComponent],
     }).compileComponents();
   });
