ba320022811465d689f97eee54152a3a1c3e2e80
Santiago Caroprese <scaroprese@applaudostudios.com>
2023-03-29 18:47:54 -0500

    Feat(Roles): add roles crud feature

--- packages/state-management-backend/src/app/users/users.module.ts
+++ packages/state-management-backend/src/app/users/users.module.ts
@@ -4,11 +4,13 @@ import { UsersController } from './controllers/users.controller';
 import { User } from './entities/user.entity';
 import { DBModule } from '../database/database.module';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { RoleController } from './controllers/roles.controller';
+import { RolesService } from './services/role.service';
 
 @Module({
   imports: [DBModule, MikroOrmModule.forFeature([User])],
-  controllers: [UsersController],
-  providers: [UsersService],
+  controllers: [UsersController, RoleController],
+  providers: [UsersService, RolesService],
   exports: [UsersService],
 })
 export class UsersModule {}
