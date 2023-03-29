10ea52c74909c411dbefb2e8174f4fb123f04434
Josue Paiz <jpaiz@applaudostudios.com>
2023-03-29 10:06:46 -0600

    Removing unnecessary import

--- packages/state-management-backend/src/app/branches/branches.module.ts
+++ packages/state-management-backend/src/app/branches/branches.module.ts
@@ -2,7 +2,6 @@ import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { BusinessModule } from '../business/business.module';
 import { Business_HQ } from '../business/entities/business.entity';
-import { BusinessService } from '../business/services/business.service';
 import { BranchesController } from './controllers/branches.controller';
 import { BusinessBranch } from './entities/businessBranch.entity';
 import { BranchesService } from './services/branches.service';
