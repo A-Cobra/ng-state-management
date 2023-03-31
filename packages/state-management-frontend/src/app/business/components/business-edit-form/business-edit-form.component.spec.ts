9ee6d451c09ed301fadf1074d3ca66ce7fe93551
Conrad Bravo <cbravo@applaudostudios.com>
2023-03-30 19:05:37 -0500

    The import wasn't made correctly, now the test whould pass

--- packages/state-management-frontend/src/app/business/components/business-edit-form/business-edit-form.component.spec.ts
+++ packages/state-management-frontend/src/app/business/components/business-edit-form/business-edit-form.component.spec.ts
@@ -1,10 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ModalService } from '@clapp1/clapp-angular';
 import { defaultBusinessClassificationBackendData } from '../../utils/default-business-classification-backend-data';
-import { defaultBusinessData } from '../../../core/utils/default-business-data';
 import { MockModalService } from '../../models/mock-modal-service.interface';
 
 import { BusinessEditFormComponent } from './business-edit-form.component';
+import { defaultBusinessData } from '../../utils/default-business-data';
 
 describe('BusinessEditFormComponent', () => {
   let component: BusinessEditFormComponent;
