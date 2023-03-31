0d35778eeece69ad23ad84e9b30a82cd52882ef1
dasanchez11 <dasrugeles@applaudostudios.com>
2023-03-31 13:41:21 -0500

    remove own pagination interface no longer needed

--- packages/state-management-backend/src/app/customers/interfaces/pagination.interface.ts
+++ packages/state-management-backend/src/app/customers/interfaces/pagination.interface.ts
@@ -2,12 +2,3 @@ export interface Pagination {
   page: number;
   limit: number;
 }
-
-export interface PaginatedResult<T> {
-  data: T;
-  metaData: {
-    currentPage: number;
-    totalItems: number;
-    totalPages: number;
-  };
-}
