6c6aeefb42e6e4fea61ce56f2fc2b855f48dbc95
Conrad Bravo <cbravo@applaudostudios.com>
2023-03-31 19:38:01 -0500

    Added the first test for the business-edit-form-component
    There are still several more to be made

--- packages/state-management-frontend/src/app/business/components/business-edit-form/business-edit-form.component.spec.ts
+++ packages/state-management-frontend/src/app/business/components/business-edit-form/business-edit-form.component.spec.ts
@@ -1,14 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ModalService } from '@clapp1/clapp-angular';
+import {
+  ClappButtonModule,
+  ClappImageDisplayModule,
+  ModalService,
+} from '@clapp1/clapp-angular';
 import { defaultBusinessClassificationBackendData } from '../../utils/default-business-classification-backend-data';
 import { MockModalService } from '../../models/mock-modal-service.interface';
 
 import { BusinessEditFormComponent } from './business-edit-form.component';
 import { defaultBusinessData } from '../../utils/default-business-data';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { ReactiveFormControlTextInputComponent } from '../../../shared/components/reactive-form-control-text-input/reactive-form-control-text-input.component';
+import { isALoadableImageUrl } from '../../../core/utils/is-a-displayable-image-url';
 
 describe('BusinessEditFormComponent', () => {
   let component: BusinessEditFormComponent;
   let fixture: ComponentFixture<BusinessEditFormComponent>;
+  let debugElement: DebugElement;
   let mockModalService: MockModalService;
 
   beforeEach(async () => {
@@ -18,6 +27,11 @@ describe('BusinessEditFormComponent', () => {
     };
     await TestBed.configureTestingModule({
       declarations: [BusinessEditFormComponent],
+      imports: [
+        ClappButtonModule,
+        ClappImageDisplayModule,
+        ReactiveFormControlTextInputComponent,
+      ],
       providers: [
         {
           provide: ModalService,
@@ -28,6 +42,7 @@ describe('BusinessEditFormComponent', () => {
 
     fixture = TestBed.createComponent(BusinessEditFormComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
     // Assigning data so that we can test just this component
     component.businessData = defaultBusinessData;
     component.id = 5;
@@ -38,5 +53,17 @@ describe('BusinessEditFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should enable all controls once we press the edit button', () => {
+    const editButton = debugElement.queryAll(By.css('clapp-button'))[1];
+    editButton.triggerEventHandler('click', null);
+
+    const allEnabled = Object.keys(component.businessFormEdit.controls).every(
+      (controlKey: string) => {
+        return component.businessFormEdit.get(controlKey)?.enabled;
+      }
+    );
+    expect(allEnabled).toBe(true);
   });
 });
