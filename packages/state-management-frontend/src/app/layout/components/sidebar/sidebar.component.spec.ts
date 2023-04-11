f1b1ca23827335d90fd1518fc062ba5454f4bcf5
Esteban Manrupe <emanrupe@applaudostudios.com>
2023-04-11 10:34:41 -0300

    fix test

--- packages/state-management-frontend/src/app/layout/components/sidebar/sidebar.component.spec.ts
+++ packages/state-management-frontend/src/app/layout/components/sidebar/sidebar.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SidebarComponent } from './sidebar.component';
+import { LayoutService } from '../../services/layout.service';
 
 describe('SidebarComponent', () => {
   let component: SidebarComponent;
@@ -9,6 +10,7 @@ describe('SidebarComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SidebarComponent],
+      providers: [{ provide: LayoutService }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SidebarComponent);
