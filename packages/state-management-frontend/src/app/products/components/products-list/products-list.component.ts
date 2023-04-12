732d1827a387e1a478b8815fd5dce680bb779f48
Conrad Bravo <cbravo@applaudostudios.com>
2023-04-12 16:29:56 -0500

    Modified the component so it simulates receivbing an observbable

--- packages/state-management-frontend/src/app/products/components/products-list/products-list.component.ts
+++ packages/state-management-frontend/src/app/products/components/products-list/products-list.component.ts
@@ -1,6 +1,7 @@
 import { Location } from '@angular/common';
 import { Component } from '@angular/core';
 import { ProductInterface } from '@state-management-app/types';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-products-list',
@@ -8,35 +9,37 @@ import { ProductInterface } from '@state-management-app/types';
   styleUrls: ['./products-list.component.scss'],
 })
 export class ProductsListComponent {
-  productsList: ProductInterface[] = [
-    {
-      idProduct: '1',
-      productName: 'name1',
-      description: 'description1',
-      price: 1,
-      discount: 5,
-      stock: 500,
-      status: 'available',
-    },
-    {
-      idProduct: '2',
-      productName: 'name2',
-      description: 'description2',
-      price: 6,
-      discount: 12,
-      stock: 400,
-      status: 'available',
-    },
-    {
-      idProduct: '3',
-      productName: 'name3',
-      description: 'description3',
-      price: 30,
-      discount: 20,
-      stock: 10,
-      status: 'available',
-    },
-  ];
+  productsList$: Observable<ProductInterface[]> =
+    // of();
+    of([
+      {
+        idProduct: '1',
+        productName: 'name1',
+        description: 'description1',
+        price: 1,
+        discount: 5,
+        stock: 500,
+        status: 'available',
+      },
+      {
+        idProduct: '2',
+        productName: 'name2',
+        description: 'description2',
+        price: 6,
+        discount: 12,
+        stock: 400,
+        status: 'available',
+      },
+      {
+        idProduct: '3',
+        productName: 'name3',
+        description: 'description3',
+        price: 30,
+        discount: 20,
+        stock: 10,
+        status: 'available',
+      },
+    ]);
   paginationConfiguration = {
     recordsPerPage: 40,
     totalRecords: 201,
