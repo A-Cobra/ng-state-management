cae6ef798691a0c994f6162858acb0fd10706ee5
Bryan Lazo <blazo@applaudostudios.com>
2023-04-21 12:28:46 -0600

    Removing useless import

--- packages/state-management-frontend/src/app/customers/components/customer-details/customer-details.component.spec.ts
+++ packages/state-management-frontend/src/app/customers/components/customer-details/customer-details.component.spec.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute, Router, Routes } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import { of, throwError } from 'rxjs';
+import { throwError } from 'rxjs';
 import { CustomerDetailsComponent } from './customer-details.component';
 import { ConfirmationModalComponent } from '../../../shared/components/confirmation-modal/confirmation-modal.component';
 import { CustomersService } from '../../services/customers.service';
