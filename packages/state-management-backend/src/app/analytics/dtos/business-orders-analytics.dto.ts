d48c9a2a612441d902a925335fea0a2196c0e363
Santiago Caroprese <scaroprese@applaudostudios.com>
2023-04-21 15:29:29 -0500

    fix: added swagger decorator to  analytics dto

--- packages/state-management-backend/src/app/analytics/dtos/business-orders-analytics.dto.ts
+++ packages/state-management-backend/src/app/analytics/dtos/business-orders-analytics.dto.ts
@@ -1,19 +1,24 @@
 import { IsBoolean, IsDate, IsIn, IsOptional, IsString } from 'class-validator';
 import { PaginationDto } from '../../common/dtos/pagination.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class BusinessOrdersAnalyticsDto extends PaginationDto {
+  @ApiProperty()
   @IsOptional()
   @IsString()
   searchValue?: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsBoolean()
   sortAsc?: boolean = true;
 
+  @ApiProperty()
   @IsOptional()
   @IsDate()
   startTime?: Date;
 
+  @ApiProperty()
   @IsOptional()
   @IsDate()
   endTime?: Date = new Date();
--- packages/state-management-backend/src/app/analytics/dtos/business-sales-analytics.dto.ts
+++ packages/state-management-backend/src/app/analytics/dtos/business-sales-analytics.dto.ts
@@ -1,23 +1,29 @@
 import { IsDate, IsOptional, IsString } from 'class-validator';
 import { PaginationDto } from '../../common/dtos/pagination.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class BusinessSalesAnalyticsDto extends PaginationDto {
+  @ApiProperty()
   @IsOptional()
   @IsString()
   searchValue?: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsString()
   businessId?: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsString()
   category?: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsDate()
   startTime?: Date;
 
+  @ApiProperty()
   @IsOptional()
   @IsDate()
   endTime?: Date = new Date();
