b27b4de8bef67487b6f1bf9d16e41d4cee86db84
Bryan Lazo <blazo@applaudostudios.com>
2023-04-24 13:09:39 -0600

    Fixing test and routes

--- packages/state-management-frontend/src/app/user/user-routing.module.ts
+++ packages/state-management-frontend/src/app/user/user-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
 const routes: Routes = [
   {
-    path: 'profile',
+    path: '',
     component: UserProfileComponent,
   },
 ];
--- packages/state-management-frontend/src/app/user/user.module.ts
+++ packages/state-management-frontend/src/app/user/user.module.ts
@@ -1,17 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { UserService } from './services/user.service';
+import { UserRoutingModule } from './user-routing.module';
+import { LoaderComponent } from '../shared/components/loader/loader.component';
 import {
   ClappButtonModule,
   ClappInputHelpersModule,
   ClappTextInputModule,
   ModalService,
 } from '@clapp1/clapp-angular';
-
-import { LoaderComponent } from '../shared/components/loader/loader.component';
-import { UserProfileComponent } from './components/user-profile/user-profile.component';
-import { UserService } from './services/user.service';
-import { UserRoutingModule } from './user-routing.module';
 
 const clappModules = [
   ClappTextInputModule,
